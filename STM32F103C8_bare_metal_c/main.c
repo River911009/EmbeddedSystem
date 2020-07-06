5407db4c6812fd7033ef50597e1cdf02bd7de2a9
River911009 <47097047+River911009@users.noreply.github.com>
2020-07-06 09:38:11 +0800

    Add files via upload
    STM32 bare metal

---
+++ STM32F103C8_bare_metal_c/main.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+// register address
+#define RCC_BASE      0x40021000
+#define GPIOC_BASE    0x40011000
+#define RCC_APB2ENR   *(volatile uint32_t *)(RCC_BASE   + 0x18)
+#define GPIOC_CRH     *(volatile uint32_t *)(GPIOC_BASE + 0x04)
+#define GPIOC_ODR     *(volatile uint32_t *)(GPIOC_BASE + 0x0C)
+// bit fields
+#define RCC_IOPCEN   (1<<4)
+#define GPIOC13      (1UL<<13)
+
+void main(void)
+{
+  RCC_APB2ENR |= RCC_IOPCEN;
+  GPIOC_CRH   &= 0xFF0FFFFF;
+  GPIOC_CRH   |= 0x00200000;
+
+  while(1)
+  {
+    //GPIOC_ODR |=  GPIOC13;            // off
+    *(volatile uint32_t *)(0x40011000 + 0x0C) |= (1UL<<13);
+    for (int i = 0; i < 900000; i++); // arbitrary delay
+    //GPIOC_ODR &= ~GPIOC13;            // on
+    *(volatile uint32_t *)(0x40011000 + 0x0C) &= ~(1UL<<13);
+    for (int i = 0; i < 100000; i++); // arbitrary delay
+  }
+}
+
